8b02a84d5039010d41f74d8643a8ed860832652a
Anna Garmashevska <anniegemail@gmail.com>
2015-11-30 17:18:38 +0200

    lesson 10-11

---
+++ GOIT/javascript/home/lesson10-11/lesson10/js/main.js
@@ -0,0 +1,19 @@
+( function() {
+	do {
+		var num = prompt('Введите число или нажмите ESC:');
+		
+		var myPow = function(number) {
+			if (!isNaN(number)) {
+				return number*number;
+			} else return -1;
+		}
+
+		var number = myPow(+num);
+		if ((num !== null)&&(num !== "")) {
+			number>= 0 ? console.log("Результат " + num+ "x" + num + "=" + myPow(num)) : console.log("Вы ввели некорректные дааные.");
+		}
+	} while (((num !== null)&&(num !== "")));
+
+	console.log('Вы вышли из программы');
+
+})();
