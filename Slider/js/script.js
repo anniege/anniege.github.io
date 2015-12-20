8644ed1556b547b5756c813dc27990dc2a3f3e54
Anna Garmashevskaya <anniegemail@gmail.com>
2015-12-21 00:37:01 +0200

    slider

--- Slider/js/script.js
+++ Slider/js/script.js
@@ -4,13 +4,14 @@
 	var pings = document.querySelector('.slider-pages');
 	var ping = document.querySelectorAll('.slider-pages-item');
 	var texts = document.querySelectorAll('.text_item');
+	var left = document.querySelector('.left');
+	var right = document.querySelector('.right');
 
-	// [].reverse.call(slides);
-	// console.log(document.querySelector('.slider-inner').innerHTML);
 
 	var SliderApp = function(defaultItem) {
 		var imgs = [],
-		globalId,
+			globalId,
+			index,
 			timer;
 
 		var slidesLength = slides.length;
@@ -19,33 +20,29 @@
 			slide(+defaultItem);
 		}
 
-		// function startAnimSliders() {
-		// 	timer = setInterval(function(){
-
-		// 	}, 1000);
-		// }
 		this.next = function() {
-			var index = globalId + 1;
+			index = globalId + 1;
 			if (index < slidesLength) {
 				slide(index);
 			} else {
-				index = 0
+				index = 0;
+				slide(index);
 			}
 		}
 
 		this.prev = function() {
-			console.log('index = ' + index);
-			var index = globalId - 1;
+			index = globalId - 1;
 			if (index >= 0) {
 				slide(index);
 			} else {
 				index = slidesLength - 1;
+				slide(index);
 			}
 		}
 
 
 		function removeActiveClass(items, customClass) {
-			[].forEach.call(items, function(item, i, arrSlider) {
+			[].forEach.call(items, function(item) {
 				item.classList.remove(customClass);
 			});
 		}
@@ -53,8 +50,7 @@
 		function slide(number) {
 			number = +number;
 			globalId = number;
-			console.log();
-				removeActiveClass(ping, 'active');
+			removeActiveClass(ping, 'active');
 			[].forEach.call(slides, function(item, i, arrSlider) {
 
 				if (i < number) {
@@ -63,18 +59,17 @@
 					arrSlider[i].style.height = '100%';
 					arrSlider[i].style.margin = '0px';
 					arrSlider[i].style.opacity = '0';
-					arrSlider[i].style.webkitTransform = 'translateX(-190px)';
+					arrSlider[i].style.webkitTransform = 'translateX(-267px)';
 					texts[i].style.visibility = 'hidden';
 				} 
 
 				if (i === number) {
 					arrSlider[i].classList.add('activeItem');
-						// console.log('i = ' + i + ', ' + 'z-index = ' + arrSlider[i].style.zIndex);
 						arrSlider[i].style.width = '100%';
 						arrSlider[i].style.height = '100%';
 						arrSlider[i].style.margin = '0px';
 						arrSlider[i].style.opacity = '1';
-						arrSlider[i].style.webkitTransform = 'translateX(-190px)';
+						arrSlider[i].style.webkitTransform = 'translateX(-267px)';
 						texts[i].style.visibility = 'visible';
 						ping[i].classList.add ('active');
 				}
@@ -95,24 +90,18 @@
 		slideBlock.addEventListener('click', function(e) {
 			e = e&&event || window.event;
 			var target = e.target || e.srcElement;
-				// console.log(target);
-				// console.log(target.className);
 			if (target.className === 'slider-pages-item') {
 				var pageNumber = target.getAttribute('data-slide-to');
 				target.classList.add ('active');
 				slide(pageNumber);
 			}
 
-			console.log(target);
 			if ((target.parentNode.className === 'left')||(target.parentNode.className === 'right')) {
 				var action = target.parentNode.getAttribute('data-slidecontrol');
-					console.log(target);
-					console.log(action);
 				if (action) {
 					self[action]();
 		  		}
 			}
-			// console.log(pageNumber);
 		});
 	}
 
