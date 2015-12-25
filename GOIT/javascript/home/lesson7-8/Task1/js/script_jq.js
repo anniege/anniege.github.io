0d2f6f55a9f5d7e631b9ebb92d3ef3414fc94f51
Anna Garmashevskaya <anniegemail@gmail.com>
2015-12-25 18:30:55 +0200

    changes to lesson7

--- GOIT/javascript/home/lesson7-8/Task1/js/script_jq.js
+++ GOIT/javascript/home/lesson7-8/Task1/js/script_jq.js
@@ -1,14 +1,14 @@
 $(function(){
-	$("#tabs").find("#tabs-1").animate({ opacity: 1, backgroundColor: jQuery.Color({ alpha: 0.1 }) }, 1000);
+	$("#tabs").find("#tabs-1").animate({ opacity: 1, backgroundColor: jQuery.Color({ alpha: 0.1 }) }, 1000).css("z-index","999");
 	$("[href = '#tabs-1']").addClass('current');
 
 
 	$("[href]").click(function(){
-		$("#tabs").find("[id |= 'tabs']").animate({ opacity: 0, backgroundColor: '#7BA05B' }, 0);
+		$("#tabs").find("[id |= 'tabs']").animate({ opacity: 0, backgroundColor: '#7BA05B' }, 0).css("z-index","-1");
 		$("#tabs a").removeClass('current');
 		var targetPage = "#" + $(this).attr("href").slice(1);
 		$(this).addClass('current');
-		$("#tabs").find(targetPage).animate({ opacity: 1, backgroundColor: jQuery.Color({ alpha: 0.1 }) }, 1000);
+		$("#tabs").find(targetPage).animate({ opacity: 1, backgroundColor: jQuery.Color({ alpha: 0.1 }) }, 1000).css("z-index","999");
 		
 	});
 });
