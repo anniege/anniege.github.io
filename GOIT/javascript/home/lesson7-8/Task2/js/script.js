a79df4a8ebeb406f713bd6549a043397d1980652
Anna Garmashevska <anniegemail@gmail.com>
2015-12-28 10:51:24 +0200

    last changes to lesson 8

--- GOIT/javascript/home/lesson7-8/Task2/js/script.js
+++ GOIT/javascript/home/lesson7-8/Task2/js/script.js
@@ -1,20 +1,22 @@
 $(function() {
 	var t0, t1, x0, y0, x1, y1;
 
-$("input").each(function(){
-	var msg = $(this).attr("title");
-	$(this).removeAttr("title");
-	var tooltip = $("<p></p>").text(msg).addClass("tooltip");
-	$(this).after(tooltip);
-});
+	$("input").each(function(){
+		var msg = $(this).attr("title");
+		$(this).removeAttr("title");
+		var tooltip = $("<p></p>").text(msg).addClass("tooltip");
+		$(this).after(tooltip);
+	});
+	
 
 $("input").mouseenter(function(){
 		$("fieldset div").css("position", "");
 		t0 = new Date();
 		x0=event.pageX;
 		y0=event.pageY;
-		$(".tooltip").stop().animate({ "opacity": 0 },0).css({"z-index": -1});
-		$(this).siblings(".tooltip").css({"top":y0, "left":x0, "z-index": "999"}).stop().animate({ "opacity": 1 },400); 
+		$(".tooltip").stop().animate({ "opacity": 0 }, 0).css({"z-index": -1});
+		$(this).siblings(".tooltip").css({"top":y0, "left":x0, "z-index": "999"}).animate({ "opacity": 1 },100); 
+
 	}).mousemove(function(){
 		t1 = new Date();
 		x1=event.pageX;
@@ -22,27 +24,28 @@ $("input").mouseenter(function(){
 		var delta = t1-t0;
 		var speed = Math.sqrt(Math.pow(x0 - x1, 2) + Math.pow(y0 - y1, 2)) / delta;
 		if (speed < 0.1) {
-			console.log(speed);
-			$(this).siblings(".tooltip").css({"top":y1, "left":x1, "z-index": "999"}).stop().animate({ "opacity": 1 },400); 
+			$(this).siblings(".tooltip").css({"top":y1, "left":x1, "z-index": "999"}).animate({ "opacity": 1 }, 400); 
 			var self = this;
 			setTimeout(function() {
-				$(self).siblings(".tooltip").stop().animate({ "opacity": 0 }, 0).css({"z-index": -1});
-			}, 1000);
+				$(self).siblings(".tooltip").stop().animate({ "opacity": 0 }, 200).stop().css({"z-index": -1});
+			}, 700);
 		}
-	}).mouseleave(function() {
-		$(this).siblings(".tooltip").stop().animate({ "opacity": 0 }, 0).css({"z-index": -1});
-	});
 
+	}).mouseleave(function() {
+		$(this).siblings(".tooltip").animate({ "opacity": 0 }, 0).stop().css({"z-index": -1});
+	});
 
 
 	$("<button>").text("Show help").insertAfter("form").click(function() {
 		$("fieldset div").css("position","relative");
-		$(".tooltip").each(function(){
-			$(this).stop().css({"top":" -5px", "left": "225px", "z-index": "999"}).animate({ "opacity": 1 }, 400);
-			var self = this;
-			setTimeout(function() {
-				$(self).stop().animate({ "opacity": 0 }, 1).css("z-index", "-1");
-			}, 4000);
-		});
+		setTimeout(function(){
+			$(".tooltip").each(function(){
+				$(this).stop().css({"top":" -5px", "left": "260px", "z-index": "999"}).css("opacity", 1);
+				var self = this;
+				setTimeout(function() {
+					$(self).stop().animate({ "opacity": 0 }, 0).css("z-index", "-1");
+				}, 1500);
+			});
+		}, 500);
 	});
 });
