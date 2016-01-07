3a2a42c9946253b29a7ae905a4b8675dbb04269f
Anna Garmashevskaya <anniegemail@gmail.com>
2016-01-07 23:29:43 +0200

    lesson9

--- GOIT/javascript/home/lesson9-10/Task1/js/script.js
+++ GOIT/javascript/home/lesson9-10/Task1/js/script.js
@@ -32,4 +32,26 @@ $(function(){
 			$(this).removeClass('active');
 		})
 		.jcarouselPagination();
+
+	 // $('.jcarousel').jcarouselAutoscroll({
+  //     interval: 3000,
+  //     target: '+=1',
+  //     autostart: true
+  // 	});
+
+
+	$('.mySelectBoxClass').customSelect();
+
+	$('.mySelectBoxClass').change(function(){
+		var index = $(this).find('option:selected').val();
+		if (+index) $('.jcarousel').jcarousel('scroll', index-1);
+	});
+
+// var params = {
+// 		changedEl: "select"
+// 	}
+// 	cuSel(params);
+
+ $('.myCheckbox').styler();
+
 });
