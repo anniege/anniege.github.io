76fba0dbbebd704600c84fa91551eb5f77241f0a
Anna Garmashevskaya <anniegemail@gmail.com>
2016-01-18 00:08:39 +0200

    lesson11-12

---
+++ GOIT/javascript/home/lesson11-12/Task2/js/script.js
@@ -0,0 +1,28 @@
+(function() {
+	document.addEventListener('DOMContentLoaded', loadContent);
+
+	function loadContent() {
+		var data = {
+			name: "Гармашевская Анна Александровна",
+			photo_url: "img/photo.jpg",
+			job_title: "Менеджер по гарантиям в Автомоторс",
+			motivation: "Хочу учить frontend потому что:",
+			motive_list: ["возможность самореализовываться",
+						  "найти инетресную работу",
+						  "работать по специальности",
+						  "дает возможность все время учится и познавать новое"],
+			phone_title: "Мой контактный телефон",
+			phone_number:"+380 67 4990705",
+			facebook_title: "Мой профиль в facebook",
+			facebook_address: "https://www.facebook.com/Garmashevskaya",
+			facebook_url: "facebook.com",
+			feedback_title: "Мой feedback:",
+			feedback_mail: "mailto:anniegemail@gmail.com",
+			feedback: "mailto:anniegemail@gmail.com"
+		};
+		var target = document.getElementById("results");
+		var profile = document.getElementById("profile").innerHTML;
+		target.innerHTML = tmpl(profile, data);
+	}
+
+})();
