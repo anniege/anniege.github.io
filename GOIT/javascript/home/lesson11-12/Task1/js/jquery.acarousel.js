aeab9a5ad8c8264cd64efd171d93980404810f3e
Anna Garmashevska <anniegemail@gmail.com>
2016-01-19 11:05:43 +0200

    finish lesson11-12

--- GOIT/javascript/home/lesson11-12/Task1/js/jquery.acarousel.js
+++ GOIT/javascript/home/lesson11-12/Task1/js/jquery.acarousel.js
@@ -2,15 +2,23 @@
 	var defaults = {
 		offsetX: 0.05,
 		offsetY: 0.08,
-		count: 4
+		count: 4,
+		scrollWrap: false,
+		animate: false,
+		animateSpeed: 3000
 	}
 
 	var options;
 
 	$.fn.acarousel = function(params) {
 
+		//extend the default options with custom
 		options = $.extend({}, defaults, options, params);
 
+		var $timerId,
+			$flag = false;
+
+		//calculation the width of acarousel-container depending on quantity of vivsible elements
 		var $widthElem = $('.acarousel-element').css('width');
 		$widthElem = parseInt($widthElem) + 25;
 
@@ -18,9 +26,9 @@
 		$('.acarousel-container').css("width", $widthContainer);
 		$('.acarousel-hider').css("width", $widthContainer);
 
-		var $page_y = $(window).height(),
-			$page_x = $(window).width();
 
+
+		//scroll with click on LEFT and RIGHT controls
 		var $elems = this;
 		var $leftControl = $('.acarousel-arrow-left'),
 			$rightControl = $('.acarousel-arrow-right');
@@ -31,40 +39,101 @@
 		var $minimumOffset = - (($elementsCount - options.count) * $pixelsOffset);
 		var $maximumOffset = 0;
 	 
-		$leftControl.click(function() {    
+		var scrollLeft = function() {    
 			if ($currentLeftValue != $maximumOffset) {
 				$currentLeftValue += $widthElem;
 				$elems.animate({ left : $currentLeftValue + "px"}, 500);
-			}        
-		});
-	 
-		$rightControl.click(function() {  
+			} else {
+				if (options.scrollWrap === true) {
+					$currentLeftValue = - (($elementsCount - options.count) * $pixelsOffset);
+					$elems.animate({ left : $currentLeftValue + "px"}, 300);
+				}
+			}       
+		}
+
+		var scrollRight = function() {  
 			if ($currentLeftValue != $minimumOffset) {
 				$currentLeftValue -= $widthElem;
 				$elems.animate({ left : $currentLeftValue + "px"}, 500);
-			}        
+			}  else {
+				if (options.scrollWrap === true) {
+					$currentLeftValue = 0;
+					$elems.animate({ left : $currentLeftValue + "px"}, 300);
+				}
+			}      
+		}
+	 
+		$leftControl.click(function() {
+			pauseAnimation(scrollLeft);
 		});
 
+		$rightControl.click(function() {
+			pauseAnimation(scrollRight);
+		});
+
+
+		//animation with the autoscroll from right to left
+		function startAnimation() { 
+			$timerId = setTimeout(function tick() {
+			scrollRight();
+			$timerId = setTimeout(tick, options.animateSpeed);
+			}, options.animateSpeed);
+		}
+
+		function stopAnimation() {
+			if ($timerId) clearTimeout($timerId);
+		}
+
+		function pauseAnimation(customFunction) {
+			if ((options.animate)&&($timerId)) {
+				stopAnimation();
+			}
+			customFunction();
+			if (options.animate) {
+				startAnimation();
+			}
+		}
+
+		if (options.animate === true) {
+			startAnimation();
+			flag = true;
+		} else {
+			stopAnimation();
+			flag = false;
+		}
+
+
+		//show large preview of the photo with click on it
+		var $page_y = $(window).height(),
+			$page_x = $(window).width();
+			
 		this.on('click', function(e) {
 			var e = e&&event || event.srcElement;
 			var $target = $(e.target);
 			var $parent = $target.parent();
 
 			if (($parent)&&($parent.hasClass('acarousel-element'))) {
+				if ((options.animate)&&($timerId)) {
+					stopAnimation();
+				}
 				var $widthImg = $page_x-options.offsetX*$page_x;
 				var $heightImg = $page_y-options.offsetY*$page_y;
-				var marginLeftImg = $widthImg/2 - ($widthImg/2)%1;
-				var marginTopImg = $heightImg/2 - ($heightImg/2)%1;
-				var $newImg = $('<div class="acarousel-large"><img src="'+ $parent.find('img').attr('src')+'" style= "width:'+ $widthImg +'px; max-height:'+ $heightImg +'px; margin-left:-'+ marginLeftImg +'px; margin-top:-'+ marginTopImg +'px"></div>');
+				var $marginLeftImg = $widthImg/2 - ($widthImg/2)%1;
+				var $marginTopImg = $heightImg/2 - ($heightImg/2)%1;
+				var $newImg = $('<div class="acarousel-large"><img src="'+ $parent.find('img').attr('src')+'" style= "width:'+ $widthImg +'px; max-height:'+ $heightImg +'px; margin-left:-'+ $marginLeftImg +'px; margin-top:-'+ $marginTopImg +'px"></div>');
 				$('body').append($newImg);
 				$newImg.on('click', function() {
 					$newImg.remove();
+					if (options.animate) {
+						startAnimation();
+					}
 				});
 			}
 		});
 
+
 		return this;
-  };
+	};
 
 })(jQuery);
 
--- GOIT/javascript/home/lesson11-12/Task1/js/script.js
+++ GOIT/javascript/home/lesson11-12/Task1/js/script.js
@@ -2,7 +2,10 @@ $(function() {
 
 $('.acarousel-list').acarousel({
 	offsetX: 0.5,
-	count: 3
+	count: 3,
+	scrollWrap: true,
+	animate: true,
+	animateSpeed: 2000
 });
 
 });
