dfeaf21ee77271b0bf0c9eaa78be1ded4cf6254f
Anna Garmashevska <anniegemail@gmail.com>
2016-01-26 16:01:59 +0200

    finish lesson 13-14

--- GOIT/javascript/home/lesson13-14/js/script.js
+++ GOIT/javascript/home/lesson13-14/js/script.js
@@ -1,127 +1,130 @@
 (function() {
-"use strict";
-
-var body = document.querySelector('body');
-var rightAnswers;
+	"use strict";
 
 
-function render(test) {
-	var target = document.getElementById("resultTest");
-	var testFunc = _.template(document.getElementById("test_Id").innerHTML);
-	target.innerHTML = testFunc(test);
-	return test;
-}
+	var body = document.querySelector('body');
+	var rightAnswers;
 
 
-function createModal() {
-	var modal = document.createElement('div');
-	var modalInner = document.createElement('div');
-	var modalClose = document.createElement('a');
-	var modalTitle = document.createElement('h2');
-	var modalResult = document.createElement('p');
-	var btnClose = document.createElement('button');
-		
-		modal.classList.add('modal');
-		modalInner.classList.add('modal-inner');
-		modalTitle.classList.add('modal_title');
-		modalResult.classList.add('modal_result');
-		btnClose.classList.add('modal_button');
-		modalClose.classList.add('modal_close');
-		
-		modalTitle.innerHTML = "Результат теста";
-		modalResult.innerHTML = "Количество правильных ответов - ";
-		btnClose.innerHTML = "Закрыть";
-		modalClose.href ="#close";
-		modalClose.title="Закрыть";
-		modalClose.innerHTML = "X";
 
-	var targetNode = document.querySelector('.test_list');
-	var cloneItem = targetNode.cloneNode(true);
-	var inputs = cloneItem.querySelectorAll('.wrapper .test_item_question');
-	[].forEach.call(inputs, function(input) {
-		input.setAttribute('disabled', 'disabled');
-	});
-	var liItems = cloneItem.querySelectorAll('.test_item');
-	[].forEach.call(liItems, function(liItem) {
-		liItem.classList.add('item--modified');
-	});
-
-	modalInner.appendChild(modalTitle);
-	modalInner.appendChild(modalResult);
-	modalInner.appendChild(cloneItem);
-	modalInner.appendChild(btnClose);
-	modalInner.appendChild(modalClose);
-	modal.appendChild(modalInner);
-	body.appendChild(modal);
-	body.classList.add('hide');
-	btnClose.addEventListener('click', closeModal);
-	modalClose.addEventListener('click', closeModal);
-}
+	function render(test) {
+		var target = document.getElementById("resultTest");
+		var testFunc = _.template(document.getElementById("test_Id").innerHTML);
+		target.innerHTML = testFunc(test);
+		return test;
+	}
 
 
-function showModal() {
-	var modal = document.querySelector('.modal');
-	modal.setAttribute('style', 'display:block;');
-}
+
+	function createModal() {
+		var modal = document.createElement('div');
+		var modalInner = document.createElement('div');
+		var modalClose = document.createElement('a');
+		var modalTitle = document.createElement('h2');
+		var modalResult = document.createElement('p');
+		var btnClose = document.createElement('button');
+			
+			modal.classList.add('modal');
+			modalInner.classList.add('modal-inner');
+			modalTitle.classList.add('modal_title');
+			modalResult.classList.add('modal_result');
+			btnClose.classList.add('modal_button');
+			modalClose.classList.add('modal_close');
+			
+			modalTitle.innerHTML = "Результат теста";
+			modalResult.innerHTML = "Количество правильных ответов - ";
+			btnClose.innerHTML = "Закрыть";
+			modalClose.href ="#close";
+			modalClose.title="Закрыть";
+			modalClose.innerHTML = "X";
+
+		var targetNode = document.querySelector('.test_list');
+		var cloneItem = targetNode.cloneNode(true);
+		var liItems = cloneItem.querySelectorAll('.test_item');
+		[].forEach.call(liItems, function(liItem) {
+			liItem.classList.add('test_item_modified');
+		});
+
+		modalInner.appendChild(modalTitle);
+		modalInner.appendChild(modalResult);
+		modalInner.appendChild(cloneItem);
+		modalInner.appendChild(btnClose);
+		modalInner.appendChild(modalClose);
+		modal.appendChild(modalInner);
+		body.appendChild(modal);
+		body.classList.add('hide');
+		btnClose.addEventListener('click', closeModal);
+		modalClose.addEventListener('click', closeModal);
+	}
 
 
-function closeModal() {
-	body.classList.remove('hide');
 
-	var modal = document.querySelector('.modal');
-	if (modal) body.removeChild(modal);
-
-	var inputsChecked = document.querySelectorAll('.wrapper .test_item_question:checked');
-	[].forEach.call(inputsChecked, function(inputChecked) {
-		inputChecked.checked = false;
-	});
-}
+	function showModal() {
+		var modal = document.querySelector('.modal');
+		modal.setAttribute('style', 'display:block;');
+	}
 
 
-function createRightAnswersDB(test) {
-	var answersObject = {};
-	test.questions.forEach(function(question, index) {
-		answersObject["question"+index] = question.trueValue;
-	});
-	return answersObject;
-}
+
+	function closeModal() {
+		body.classList.remove('hide');
+
+		var modal = document.querySelector('.modal');
+		if (modal) body.removeChild(modal);
+
+		var inputsChecked = document.querySelectorAll('.wrapper .test_item_question:checked');
+		[].forEach.call(inputsChecked, function(inputChecked) {
+			inputChecked.checked = false;
+		});
+	}
 
 
-function checkAnswers(e) {
-	var e = e&&event || event.srcElement;
-	e.preventDefault();
 
-	//create the elements of modal
-	createModal();
+	function createRightAnswersDB(test) {
+		var answersObject = {};
+		test.questions.forEach(function(question, index) {
+			answersObject["question"+index] = question.trueValue;
+		});
+		return answersObject;
+	}
 
-	//create user answers database
-	var userAnswers = {};
-	var inputsChecked = document.querySelectorAll('.wrapper .test_item_question:checked');
-	var tempAnswerArr;
-	[].forEach.call(inputsChecked, function(inputChecked) {
-		if (!userAnswers[inputChecked.name]) {
-		tempAnswerArr= [];
-		tempAnswerArr.push(+inputChecked.value);
-		userAnswers[inputChecked.name] = tempAnswerArr;
-		} else {
+
+
+	function checkAnswers(e) {
+		var e = e&&event || event.srcElement;
+		e.preventDefault();
+
+		//create the elements of modal
+		createModal();
+
+		//create user answers database
+		var userAnswers = {};
+		var inputsChecked = document.querySelectorAll('.wrapper .test_item_question:checked');
+		var tempAnswerArr;
+		[].forEach.call(inputsChecked, function(inputChecked) {
+			if (!userAnswers[inputChecked.name]) {
+			tempAnswerArr= [];
 			tempAnswerArr.push(+inputChecked.value);
 			userAnswers[inputChecked.name] = tempAnswerArr;
-		}
-	});
-	//compare the user answers with right answers
-	var counterOfRightAnswers = 0; 
-	var modal = document.querySelector('.modal');
+			} else {
+				tempAnswerArr.push(+inputChecked.value);
+				userAnswers[inputChecked.name] = tempAnswerArr;
+			}
+		});
+		//compare the user answers with right answers
+		var counterOfRightAnswers = 0; 
+		var modal = document.querySelector('.modal');
 
-	for (var rightQuestion in rightAnswers) {
-		//array of right answers
-		var rightList = rightAnswers[rightQuestion];
-		//length of array of right answers
-		var rightListLength = rightAnswers[rightQuestion].length;
-		//select all checkboxes by key(question) from right answers database
-		var inputsRightQuestion = modal.querySelectorAll('input[name="'+ rightQuestion +'"]');
+		for (var rightQuestion in rightAnswers) {
+			//array of right answers
+			var rightList = rightAnswers[rightQuestion];
+			//length of array of right answers
+			var rightListLength = rightAnswers[rightQuestion].length;
+			//select all checkboxes by key(question) from right answers database
+			var inputsRightQuestion = modal.querySelectorAll('input[name="'+ rightQuestion +'"]');
 
-		//if user answered on proper question
-		if (rightQuestion in userAnswers) {
+			//if user answered on proper question
+			if (rightQuestion in userAnswers) {
 				//array of user answers
 				var userList = userAnswers[rightQuestion];
 				//length of array of user answers
@@ -150,51 +153,53 @@ function checkAnswers(e) {
 				}
 				//if all answers were correct
 				if (counterFlag) ++counterOfRightAnswers;
-		} else {
-			//highlight the correct answers missed by user
-			for (var j=0; j<rightListLength; j++) {
-				inputsRightQuestion[rightList[j]].parentNode.classList.add('correct--missed');
+			} else {
+				//highlight the correct answers missed by user
+				for (var j=0; j<rightListLength; j++) {
+					inputsRightQuestion[rightList[j]].parentNode.classList.add('correct--missed');
+				}
 			}
 		}
+		//add the result message in modal
+		var result = document.querySelector('.modal_result');
+		if (counterOfRightAnswers === 5) {
+			result.classList.add('modal_result_success');
+			result.innerHTML += counterOfRightAnswers + ". ТЕСТ ПРОЙДЕН."; 
+		} else {
+			result.innerHTML += counterOfRightAnswers + ". ТЕСТ НЕ ПРОЙДЕН."; 
+		}
+		//show the modal 
+		showModal();
+		return false;
 	}
-	//add the result message in modal
-	var result = document.querySelector('.modal_result');
-	if (counterOfRightAnswers === 5) {
-		result.classList.add('modal_result_success');
-		result.innerHTML += counterOfRightAnswers + ". ТЕСТ ПРОЙДЕН."; 
-	} else {
-		result.innerHTML += counterOfRightAnswers + ". ТЕСТ НЕ ПРОЙДЕН."; 
+
+
+
+	function clearStorage() {
+		window.localStorage.clear();
 	}
-	//show the modal 
-	showModal();
-	return false;
-}
 
 
-function clearStorage() {
-	window.localStorage.clear();
-}
+	function doTest() {
+		try {
+			//parse from localStorage
+			var test = JSON.parse(localStorage.test);
 
+			//render data from localStorage
+			render(test);
 
-function doTest() {
-	try {
-		//parse from localStorage
-		var test = JSON.parse(localStorage.test);
+			//create right answers database
+			rightAnswers = createRightAnswersDB(test);
 
-		//render data from localStorage
-		render(test);
-
-		//create right answers database
-		rightAnswers = createRightAnswersDB(test);
-
-		//add event on test submit
-		var sbmtButton = document.querySelector('[type="submit"]');
-		sbmtButton.addEventListener('click', checkAnswers);
-	} catch(e) {
-		console.log('error = ', e);
+			//add event on test submit
+			var sbmtButton = document.querySelector('[type="submit"]');
+			sbmtButton.addEventListener('click', checkAnswers);
+		} catch(e) {
+			console.log('error = ', e);
+		}
 	}
-}
 
-document.addEventListener('DOMContentLoaded', doTest);
-// document.addEventListener('beforeunload', clearStorage);
+
+	document.addEventListener('DOMContentLoaded', doTest);
+	// document.addEventListener('beforeunload', clearStorage);
 })();
