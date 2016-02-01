417c45da5dfcd2effc652a4206491d80d1c6cd11
Anna Garmashevskaya <anniegemail@gmail.com>
2016-02-01 22:43:55 +0200

    lesson15-16

--- GOIT/javascript/home/lesson15-16/lesson16/js/script.js
+++ GOIT/javascript/home/lesson15-16/lesson16/js/script.js
@@ -1,6 +1,5 @@
 ;(function(){
 	
-
 	// class Human
 	function Human(name, age, gender, height, weight) {
 		this.name = name || "Ivan";
@@ -9,7 +8,6 @@
 		this.height = height || 180;
 		this.weight = weight || 75;
 	}
-
 
 	// class Worker inherits class Human
 	function Worker(name, age, gender, height, weight, job, wage) {
@@ -24,13 +22,12 @@
 	Worker.prototype.working = function() {
 		var str = '';
 		if (this.gender === 'male') {
-			str = this.name +' is '+this.age+ 'years old and he is working as a ' + this.job;
+			str = this.name +' is '+this.age+ ' years old and he is working as a ' + this.job;
 		} else {
-			str = this.name +' is '+this.age+ 'years old and she is working as a ' + this.job;			
+			str = this.name +' is '+this.age+ ' years old and she is working as a ' + this.job;			
 		}
 		return str;
 	};
-
 	
 	// class Student inherits class Human
 	function Student(name, age, gender, height, weight, studyPlace, scholarship) {
@@ -53,17 +50,17 @@
 	var student2 = new Student("Polina", 21, "female");
 	var student3 = new Student("Inna", 23, "female", 167, 48, "KPI", 800);
 
-	console.log(worker1);
+	console.log("worker1: ", worker1);
 	console.log(worker1.working());
-	console.log(worker2);
+	console.log("worker1: ", worker2);
 	console.log(worker2.working());
-	console.log(worker3);
+	console.log("worker1: ", worker3);
 	console.log(worker3.working());
 
-	console.log(student1);
+	console.log("sudent1: ", student1);
 	console.log(student1.watchSeries());
-	console.log(student2);
+	console.log("sudent1: ", student2);
 	console.log(student2.watchSeries());
-	console.log(student3);
+	console.log("sudent1: ", student3);
 	console.log(student3.watchSeries());
 })();
