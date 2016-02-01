e590a0082c7429cf80d6c949189f81f61f5a2ab2
Anna Garmashevskaya <anniegemail@gmail.com>
2016-02-01 21:40:57 +0200

    lesson15-16

---
+++ GOIT/javascript/home/lesson15-16/lesson15/js/script.js
@@ -0,0 +1,63 @@
+$(function(){
+
+	var $googleSearch = function(index) {
+		$('.search-result').html("");
+		var urlAdded = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&key=ABQIAAAACKQaiZJrS0bhr9YARgDqUxQBCBLUIYB7IF2WaNrkYqF0tBovNBQFDtM_KNtb3xQxWff2mI5hipc3lg&q=";
+		var valueInput = encodeURIComponent($('.search__input').val());
+		if (valueInput) {
+			urlAdded +=valueInput+"&rsz=8&start="+index*8+"&callback=GoogleCallback&context=?";
+			var pages = 0;
+			$.ajax({
+				url: urlAdded,
+				dataType : "jsonp",
+				success: function (data) {
+					if (data.results.length) {
+						pages = data.cursor.pages.length;
+						$.each(data.results, function(i, val){
+							var ul = $('<ul></ul>').addClass('search-result__list');
+							var li = $('<li></li>').addClass('search-result__list__item');
+							var title = $('<h2></h2>').addClass('search-result__list__item__title');
+							var link = $('<a></a>').attr('href',val.url).addClass('search-result__list__item__link');
+							$(link).html(val.titleNoFormatting);
+							var content = $('<div></div>').addClass('search-result__list__item__content');
+							var cite = $('<cite></cite>').addClass('search-result__list__item__content__url').html(val.url);
+							var contentData = $('<p></p>').addClass('search-result__list__item__content__data').html(val.content);
+							$(content).append(cite);
+							$(content).append(contentData);
+							$(title).append(link);
+							$(li).append(title);
+							$(li).append(content);
+							$(ul).append(li);
+							$('.search-result').append(ul);  
+						}); 
+							var pagesList = $('<ol></ol>').addClass('search-result__pages');
+							for (var i=1; i<=pages; i++) {
+								var pagesItem = $('<li></li>').addClass('search-result__pages__item');
+								var pagesLink = $('<a></a>').addClass('search-result__pages__item__link').html(i);
+								if (i === (index+1)) {
+									$(pagesLink).addClass('search-result__pages__item__link--current');
+								}
+								$(pagesItem).append(pagesLink);
+								$(pagesList).append(pagesItem);
+							}
+							$('.search-result').append(pagesList); 
+							$('.search-result__pages__item__link').click(function(e) {
+								$googleSearch(e.target.innerHTML-1);
+							});
+					} else {
+							var message = $('<p></p>').addClass('search-result__message').html("На запит не знайдено жодного документа.");
+							$('.search-result').append(message);
+					}
+				}
+			});
+		}
+	}
+
+	$('.search__button').click(function() {
+			$googleSearch(0);
+	});
+});
+
+function GoogleCallback(func, data){
+	window[func](data);
+}
---
+++ GOIT/javascript/home/lesson15-16/lesson16/js/script.js
@@ -0,0 +1,69 @@
+;(function(){
+	
+
+	// class Human
+	function Human(name, age, gender, height, weight) {
+		this.name = name || "Ivan";
+		this.age = age || 25;
+		this.gender = gender || "male";
+		this.height = height || 180;
+		this.weight = weight || 75;
+	}
+
+
+	// class Worker inherits class Human
+	function Worker(name, age, gender, height, weight, job, wage) {
+		Human.apply(this, arguments);
+		this.job = job || "manager";
+		this.wage = wage || 9000;
+	}
+
+	Worker.prototype = Object.create(Human.prototype);
+	Worker.prototype.constructor = Worker;
+
+	Worker.prototype.working = function() {
+		var str = '';
+		if (this.gender === 'male') {
+			str = this.name +' is '+this.age+ 'years old and he is working as a ' + this.job;
+		} else {
+			str = this.name +' is '+this.age+ 'years old and she is working as a ' + this.job;			
+		}
+		return str;
+	};
+
+	
+	// class Student inherits class Human
+	function Student(name, age, gender, height, weight, studyPlace, scholarship) {
+		Human.apply(this, arguments);
+		this.studyPlace = studyPlace || "manager";
+		this.scholarship = scholarship || 1500;
+	}
+	Student.prototype = Object.create(Human.prototype);
+	Student.prototype.constructor = Student;
+
+	Student.prototype.watchSeries = function() {
+		return this.name +' is '+ this.age +' years old and is watching Fast\'N\'Loud and has average scholarship in amount of ' + this.scholarship;
+	};
+
+	var worker1 = new Worker("Victor");
+	var worker2 = new Worker("Olga", undefined, "female", 156, 50, "teacher");
+	var worker3 = new Worker("Kate", 44, "female", 172, 56, undefined, 15000);
+
+	var student1 = new Student(undefined, 19);
+	var student2 = new Student("Polina", 21, "female");
+	var student3 = new Student("Inna", 23, "female", 167, 48, "KPI", 800);
+
+	console.log(worker1);
+	console.log(worker1.working());
+	console.log(worker2);
+	console.log(worker2.working());
+	console.log(worker3);
+	console.log(worker3.working());
+
+	console.log(student1);
+	console.log(student1.watchSeries());
+	console.log(student2);
+	console.log(student2.watchSeries());
+	console.log(student3);
+	console.log(student3.watchSeries());
+})();
