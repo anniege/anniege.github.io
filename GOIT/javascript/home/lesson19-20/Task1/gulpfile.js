b78a8cb2d4a1e04c211ee5288dbc167725d596a3
Anna Garmashevska <anniegemail@gmail.com>
2016-02-15 17:32:24 +0200

    lesson19-20

--- GOIT/javascript/home/lesson19-20/Task1/gulpfile.js
+++ GOIT/javascript/home/lesson19-20/Task1/gulpfile.js
@@ -1,14 +1,40 @@
-"use strict";
-const gulp = require('gulp');
-const less = require('gulp-less');
-const watch = require('gulp-watch');
+var gulp = require('gulp');
+var less = require('gulp-less');
+var watch = require('gulp-watch');
+var concatCss = require('gulp-concat-css');
+var order = require("gulp-order");
+var streamqueue = require('streamqueue');
 
 gulp.task('css', function(){
-	return gulp.src('src/less/*.less')
-	.pipe(watch('src/less/*.less'))
-	.pipe(less())
-	.pipe(gulp.dest('public/css'));
+	var cssStream = gulp.src('src/css/*.css')
+		.pipe(order(["reset.css",
+					 "fonts.css"]))
+		.pipe(concatCss("default.css"));
+
+	var lessStream = gulp.src('src/css/**/*.less')
+		.pipe(less());
+
+	var mergedStream = streamqueue({ objectMode: true }, cssStream, lessStream)
+		.pipe(concatCss('style.css'))
+		// .pipe(minify())
+		.pipe(gulp.dest('public/css'));
+
+	return mergedStream;
 });
 
+gulp.task('images', function() {
+	return gulp.src('src/img/*.*')
+	.pipe(gulp.dest('public/img'));
+});
 
-gulp.task('default', ['css']);
+gulp.task('scripts', function(){
+	return gulp.src('src/js/*.js')
+	// .pipe(watch('src/js/*.js'))
+	.pipe(gulp.dest('public/js'));
+});
+
+gulp.task('watch', function() {
+	return gulp.watch('src/css/**/*.less', ['css']);
+});
+
+gulp.task('default', ['css', 'scripts', 'images', 'watch']);
---
+++ GOIT/javascript/home/lesson19-20/Task1/src/js/script.js
@@ -0,0 +1,62 @@
+(function() {
+	var info = {
+		news: {
+			title: 'latest news',
+			articles: [
+				{
+					date: {
+						day: 26,
+						month: 'Jan'
+					},
+					src: 'public/img/news_1.jpg',
+					title: 'Advanced Machinery Helps Improve Quality',
+					user: 'cmsmasters',
+					comments: 6,
+					content: 'Cum sociis natoque penatibus et magnis dis parturient ontesmus. Pro vel nibh et elit mollis commodo et nec augueique Nemo enim ipsam voluptatem quia ptas sit aspernatur samomo enim ipsam voluptatem.'
+				},
+				{
+					date: {
+						day: 26,
+						month: 'Jan'
+					},
+					src: 'public/img/news_2.jpg',
+					title: 'Powerful Techniques for Advanced Service',
+					user: 'cmsmasters',
+					comments: 3,
+					content: 'Cum sociis natoque penatibus et magnis dis parturient ontesmus. Pro vel nibh et elit mollis commodo et nec augueique Nemo enim ipsam voluptatem quia ptas sit aspernatur samomo enim ipsam voluptatem.'
+				}
+			]
+		},
+		banners: {
+			title: 'Banners Place',
+			bannerList: [
+				{
+					title: 'Accordion Panel 1',
+					content: 'Cum sociis natoque penatibus et magnis dis parturient mus. Pro vel nibh et elit mollis commodo et nec augueique Nemo enim ipsam voluptatem quia ptas sit aspernatur momo. Cum sociis natoque penatibus et magnis dis parturient ontesmus. Nemo enim ipsam voluptatem quia ptas sit aspernatur samomoPro vel nibh et elit mollis commodo et nec augueique Nemo enim ipsam voluptatem quia ptas sit aspernatur\
+	samomo enim ipsam voluptatem.'
+				},
+				{
+					title: 'Accordion Panel 2',
+					content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam non repudiandae ratione temporibus, dolor consequatur nesciunt magni rerum, consectetur esse, fugit, reprehenderit fuga quo deserunt quidem soluta vitae sed et!.'
+				},
+				{
+					title: 'Accordion Panel 3',
+					content: 'Cum sociis natoque penatibus et magnis dis enim ipsam voluptatem quia ptas sit aspernatur samomo enim ipsam voluptatem.'
+				},
+				{
+					title: 'Accordion Panel 4',
+					content: 'Cum sociis natoque penatibus et magnis dis parturient mus. Pro vel nibh et elit mollis commodo et nec augueique Nemo enim ipsam voluptatem quia ptas sit aspernatur momo. Cum sociis natoque penatibus et magnis dis parturient ontesmus. Nemo enim ipsam voluptatem quia ptas sit aspernatur samomoPro vel nibh et elit mollis commodo et nec augueique Nemo enim ipsam voluptatem quia ptas sit aspernatur.'
+				}
+			]
+		}
+	}
+
+function loadData() {
+	var newsBanners = document.querySelector('.news-banners');
+	var temp = _.template(document.getElementById('tmpl').innerHTML);
+	newsBanners.innerHTML = temp(info);
+}
+
+document.addEventListener('DOMContentLoaded', loadData);
+})();
+
