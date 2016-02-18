f71a597e2528017b81867929c3e3e1eb249209a2
Anna Garmashevska <anniegemail@gmail.com>
2016-02-18 11:00:04 +0200

    last lesson19-20

--- GOIT/javascript/home/lesson19-20/Task1/gulpfile.js
+++ GOIT/javascript/home/lesson19-20/Task1/gulpfile.js
@@ -5,6 +5,12 @@ var watch = require('gulp-watch');
 var concatCss = require('gulp-concat-css');
 var order = require("gulp-order");
 var streamqueue = require('streamqueue');
+var autoprefixer = require('gulp-autoprefixer');
+var rename = require('gulp-rename');
+var cssnano = require('gulp-cssnano');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin');
 
 gulp.task('css', function(){
 	var cssStream = gulp.src('src/css/*.css')
@@ -18,25 +24,42 @@ gulp.task('css', function(){
 
 	var mergedStream = streamqueue({ objectMode: true }, cssStream, lessStream)
 		.pipe(concatCss('style.css'))
-		// .pipe(minify())
+		.pipe(autoprefixer({
+			browsers: ['last 10 versions', '> 2%'],
+			cascade: false
+		}))
+		.pipe(cssnano())
+		.pipe(rename('style.min.css'))
 		.pipe(gulp.dest('dist/css'));
 
 	return mergedStream;
 });
 
-gulp.task('images', function() {
-	return gulp.src('src/img/*.*')
-	.pipe(gulp.dest('dist/img'));
-});
-
 gulp.task('scripts', function(){
 	return gulp.src('src/js/*.js')
 	.pipe(watch('src/js/*.js'))
+	.pipe(concat('script.js'))
+	.pipe(uglify())
+	.pipe(rename('script.min.js'))
 	.pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('images', function() {
+	return gulp.src('src/img/*.*')
+	.pipe(imagemin({
+		progressive: true,
+		interlaced: true
+	}))
+	.pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('fonts', function() {
+	return gulp.src('src/fonts/**/*.*')
+	.pipe(gulp.dest('dist/fonts'));
 });
 
 gulp.task('watch', function() {
 	return gulp.watch('src/css/**/*.less', ['css']);
 });
 
-gulp.task('default', ['css', 'scripts', 'images', 'watch']);
+gulp.task('default', ['css', 'scripts', 'images', 'fonts', 'watch']);
